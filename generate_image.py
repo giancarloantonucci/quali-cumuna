aaed742ab4a0f037a2433414b85fa7c61c06bdc7
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2024-02-18 19:48:47 +0000

    Make faster js and py

--- generate_image.py
+++ generate_image.py
@@ -7,25 +7,26 @@ from cartopy.mpl.geoaxes import GeoAxes
 from matplotlib.pyplot import subplots, savefig
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+
 def parse_string(input_string):
+    # Function to parse input strings into manageable chunks
     result = []
-    tokens = findall(r'\b(?:AG|CL|CT|EN|ME|PA|RG|SR|TP)(?:(?:\s*,)?\s*[AIV0-9]+[a-z]*(?:-\d+)?)+\b', input_string)
+    pattern = r"\b(?:AG|CL|CT|EN|ME|PA|RG|SR|TP)(?:(?:\s*,)?\s*[AIV0-9]+[a-z]*(?:-\d+)?)+\b"
+    tokens = findall(pattern, input_string)
     for token in tokens:
         header = token[:2]
-        idxs = [s.strip() for s in token[2:].split(',')]
-        for idx in idxs:
-            _match = match(r'((\d+)-(\d+))', idx)
-            if _match:
-                _, start, end = _match.groups()
-                start = int(start)
-                end = int(end)
-                for n in range(start, end + 1):
-                    result.append(f"{header} {n}")
+        indices = [s.strip() for s in token[2:].split(",")]
+        for idx in indices:
+            if "-" in idx:
+                start, end = map(int, idx.split("-"))
+                result.extend(f"{header} {n}" for n in range(start, end + 1))
             else:
                 result.append(f"{header} {idx}")
     return result
 
+
 def extract(string_or_list_of_strings, dictionary):
+    # Function to extract data from a dictionary based on input strings
     output = []
     get_name = False
     if isinstance(string_or_list_of_strings, str):
@@ -36,61 +37,92 @@ def extract(string_or_list_of_strings, dictionary):
         for string in list_of_strings:
             find_in_list(string, dictionary, output, get_name)
     return output
-    
+
+
 def find_in_list(string, _list, output, get_name):
+    # Helper function to search for strings in a list
     for _dict in _list:
         find_in_dict(string, _dict, output, get_name)
     return output
-    
+
+
 def find_in_dict(string, _dict, output, get_name):
-    if _dict['acronym'] == string:
+    # Helper function to search for strings in a dictionary
+    if _dict["acronym"] == string:
         get_name = True
-    data = _dict['data']
+    data = _dict["data"]
     if isinstance(data, list):
         find_in_list(string, data, output, get_name)
     if get_name:
-        output.append(_dict['name'])
+        output.append(_dict["name"])
     return output
 
+
+# Load data files
+riggiuni = read_file("./finaiti/riggiuni/riggiuni.shp")
+cumuna = read_file("./finaiti/cumuna/cumuna.shp")
+with open("./codenames_vs.json") as f: all_codenames = load(f)
+
+# Collect user strings
 input_string1 = argv[1]
 input_string2 = argv[2]
 input_string3 = argv[3]
 
-riggiuni = read_file("./finaiti/riggiuni/riggiuni.shp")
-cumuna = read_file("./finaiti/cumuna/cumuna.shp")
+# Process strings
+codenames = parse_string(input_string1)
+names_ita = set(extract(codenames, all_codenames))
+names_ita.update(name.strip() for name in input_string2.split(","))
+names_scn = {name.strip() for name in input_string3.split(",")}
+selected_cumuna = cumuna["ITA"].isin(names_ita) | cumuna["SCN"].isin(names_scn)
 
+# Plotting
 crs_epsg = ccrs.epsg("3857")
 riggiuni_epsg = riggiuni.to_crs(epsg="3857")
 cumuna_epsg = cumuna.to_crs(epsg="3857")
 
-f = open('./piccitto.json')
-piccitto = load(f)
-f.close()
-
-list_of_headers_plus_idxs = parse_string(input_string1)
-names = extract(list_of_headers_plus_idxs, piccitto) + [x.strip() for x in input_string2.split(',')]
-names_scn = [x.strip() for x in input_string3.split(',')]
-
-idx = (cumuna_epsg['ITA'] == "")
-for name in names:
-    idx = idx | (cumuna_epsg['ITA'] == name)
-for name in names_scn:
-    idx = idx | (cumuna_epsg['SCN'] == name)
-    
 fig, ax = subplots(subplot_kw={"projection": crs_epsg}, figsize=(7, 7))
 ax.set_extent([11.8, 15.7, 38.9, 36.6])
-ax.add_geometries(riggiuni_epsg["geometry"], crs=crs_epsg, facecolor="white", edgecolor="black", linewidth=1)
-ax.add_geometries(cumuna_epsg["geometry"][idx], crs=crs_epsg, facecolor="#2ECC71", edgecolor='black', linewidth=0.5)
+ax.add_geometries(
+    riggiuni_epsg["geometry"],
+    crs=crs_epsg,
+    facecolor="white",
+    edgecolor="black",
+    linewidth=1,
+)
+ax.add_geometries(
+    cumuna_epsg["geometry"][selected_cumuna],
+    crs=crs_epsg,
+    facecolor="#2ECC71",
+    edgecolor="black",
+    linewidth=0.5,
+)
 
-axins = inset_axes(ax, width="60%", height="60%", loc='lower left',
+axins = inset_axes(
+    ax,
+    width="60%",
+    height="60%",
+    loc="lower left",
     bbox_to_anchor=(0.085, 0.01, 0.34, 0.34),
     bbox_transform=ax.transAxes,
     axes_class=GeoAxes,
-    axes_kwargs=dict(projection=crs_epsg))
+    axes_kwargs=dict(projection=crs_epsg),
+)
 axins.set_extent([12.24, 12.96, 35.95, 35.4])
-axins.add_geometries(riggiuni_epsg["geometry"], crs=crs_epsg, facecolor="white", edgecolor="black", linewidth=1)
-axins.add_geometries(cumuna_epsg["geometry"][idx], crs=crs_epsg, facecolor="#2ECC71", edgecolor='black', linewidth=0.5)
+axins.add_geometries(
+    riggiuni_epsg["geometry"],
+    crs=crs_epsg,
+    facecolor="white",
+    edgecolor="black",
+    linewidth=1,
+)
+axins.add_geometries(
+    cumuna_epsg["geometry"][selected_cumuna],
+    crs=crs_epsg,
+    facecolor="#2ECC71",
+    edgecolor="black",
+    linewidth=0.5,
+)
 
-savefig('./public/mmaggini.png', bbox_inches='tight')
-savefig('./public/mmaggini.svg', bbox_inches='tight')
-print('mmaggini.png')
+savefig("./public/images/mmaggini.png", bbox_inches="tight")
+savefig("./public/images/mmaggini.svg", bbox_inches="tight")
+print("images/mmaggini.png")
--- server.js
+++ server.js
@@ -1,43 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PythonShell } = require('python-shell');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const publicDirectoryPath = path.join(__dirname, 'public');
+const imageDirectoryPath = path.join(publicDirectoryPath, 'images');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(publicDirectoryPath, 'index.html'));
 });
 
 app.post('/generate_image', (req, res) => {
-    const inputString1 = req.body.inputString1;
-    const inputString2 = req.body.inputString2;
-    const inputString3 = req.body.inputString3;
+    try {
+        const inputString1 = req.body.inputString1;
+        const inputString2 = req.body.inputString2;
+        const inputString3 = req.body.inputString3;
 
-    let options = {
-        mode: 'text',
-        pythonPath: 'python3',
-        scriptPath: __dirname,
-        args: [inputString1, inputString2, inputString3]
-    };
+        const options = {
+            mode: 'text',
+            pythonPath: 'python3',
+            scriptPath: __dirname,
+            args: [inputString1, inputString2, inputString3]
+        };
 
-    PythonShell.run('generate_image.py', options).then(result => {
-        const imagePath = result[0].trim();
-        res.json({ imageUrl: imagePath });
-    });
+        PythonShell.run('generate_image.py', options).then(result => {
+            const imagePath = result[0].trim();
+            res.json({ imageUrl: imagePath });
+        });
+    } catch (error) {
+        console.error('Error generating image:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.post('/download_png', (req, res) => {
-    const file = __dirname + '/public/mmaggini.png';
+    const file = path.join(imageDirectoryPath, 'mmaggini.png');
     res.download(file);
 });
 
 app.post('/download_svg', (req, res) => {
-    const file = __dirname + '/public/mmaggini.svg';
+    const file = path.join(imageDirectoryPath, 'mmaggini.svg');
     res.download(file);
 });
 
