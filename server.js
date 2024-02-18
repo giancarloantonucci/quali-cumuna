fbe79069910e1a748f9c102590821f0539f26cdc
Giancarlo A. Antonucci <giancarlo.antonucci@icloud.com>
2024-02-18 16:41:01 +0000

    Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { PythonShell } = require('python-shell');
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
